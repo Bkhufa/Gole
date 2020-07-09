1c6cbe262c6cf2aacdcf75e8555a772e56f6c9bb
Glitch (hello-express) <none>
2020-07-09 10:13:40 +0000

    🖐🎟 Checkpoint ./server.js:6579204/8

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ function handleEvent(event) {
       const groupId = event.source.groupId;
       const type = event.source.type;
             
-      answer = 'Thanks for inviting me... bitch you should be the one thanking me for looking up on your shit.\n\nI will answer you if you ask a question ending with _?\ntry: Yo mama_?'
+      answer = 'Thanks for inviting me... bitch you should be the one thanking me for looking up on your crap.\n\nI will answer you if you ask a question ending with _?\ntry: Yo mama_?'
       const message = {
           type: 'text',
           text: answer
